71f4dbbaf2f7460d926a9672112e077478161f66
Vinicius <vinicius.mlhro@gmail.com>
2022-05-17 21:31:19 -0300

    atualização do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,10 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
- export default function Home({ postsPagination }) {
+ export default function Home({ postsPagination }: HomeProps) {
+
+  const [posts, setPosts] = useState();
+
   return (
     <>
     <main className={commonStyles.container}>
@@ -58,4 +61,39 @@ interface HomeProps {
     </>
   )
  }
+
+ export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+    const postsResponse = await prismic.query([Prismic.Predicates.at('document.type', 'posts'),
+
+  ],
+  {
+    pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+      title: post.data.title,
+      substitle: post.data.subtitle,
+      author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    nextPage: postsResponse.next_page,
+    results: posts,
+  }
+
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+ };
    
